554ca8435529b55d72d8103751dced14f995ef50
quantxz <indiedev.core@gmail.com>
2024-05-15 07:27:38 -0300

    cors update

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/error-middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionsFilter());
-  await app.listen(3000);
+  app.enableCors()
+  await app.listen(3000);  
 }
 bootstrap();
